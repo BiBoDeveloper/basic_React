4f0e30a0c95a2d8fe316fbdc0b6c126f662f25a2
BiBoDeveloper <151799383+BiBoDeveloper@users.noreply.github.com>
2024-01-22 22:34:36 +0700

    22012024-001

--- basic_phare1/src/App.jsx
+++ basic_phare1/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import { useEffect } from "react";
 import "./App.css"
 import Transection from "./components/Transection";
@@ -10,6 +10,7 @@ import ReportComponent from "./components/ReportComponent";
 function App() {
   const design = { color : "red", textAlign : "center", fontSize : "1.5rem", marginBottom :  "20px"}
   const [items, setItems] = useState([]);
+
 
 const onAddNewItem = (newItem) => {
   setItems((prevItem) => {
@@ -24,28 +25,41 @@ useEffect(() => {
   const amounts = items.map((item) => item.amount);
   const income = amounts.filter(element => element > 0).reduce((total, element) => total+=element,0);
   const expense = (amounts.filter(element => element < 0).reduce((total, element) => total+=element,0)) * -1;
-  console.log('Income:: ', income);
-  console.log("🚀 ~ useEffect ~ expense:", expense);
-
 
   setReportIncome(income)
   setReportExpense(expense)
 },[items, reportExpense, reportIncome])
 
+//reducer state
+const [showReport, setShowReport] = useState(false);
+const reducer = (state, action)=> {
+      switch(action.type) {
+        case "SHOW" :
+          return setShowReport(true)
+        case "HIDE" : 
+          return setShowReport(false)
+      }
+}
+const [result, dispatch] = useReducer(reducer, showReport)
   return (
     <DataContext.Provider value={
-      {
-        income : reportIncome,
-        expense : reportExpense
-      }
-    }>
-      <div className="container">
-          <h1 style={design}>Expense-Income Management Program</h1>
-          <ReportComponent />
-          <FormComponent onAddItem = {onAddNewItem} />
-          <Transection items = {items} />
+        {
+          income : reportIncome,
+          expense : reportExpense
+        }
+      }>
+        <div className="container">
+            <h1 style={design}>Expense-Income Management Program</h1>
+            {showReport && <ReportComponent />}
+            <FormComponent onAddItem = {onAddNewItem} />
+            <Transection items = {items} />
+
+        <h1>{result}</h1>
+        <button onClick={()=>dispatch({type:"SHOW", payload: 10})}>Show</button>
+        <button onClick={()=>dispatch({type:"HIDE", payload: 5})}>Hide</button>
       </div>
     </DataContext.Provider>
+
   )
 }
 
--- basic_phare1/src/components/ReportComponent.jsx
+++ basic_phare1/src/components/ReportComponent.jsx
@@ -1,12 +1,23 @@
 import { useContext } from "react"
 import DataContext from "../data/DataContext"
+import "./ReportComponent.css"
 
 const ReportComponent = () => {
     const { income, expense } = useContext(DataContext)
     return (
         <>
-            <p>Income : {income}</p>
-            <p>Expense : {expense}</p>
+            <h4>Balance (₭)</h4>
+            <h1>{income - expense}</h1>
+            <div className="report-container">
+                <div>
+                    <h4>Total of Income</h4>
+                    <p className="report plus">₭{income}</p>
+                </div>
+                <div>
+                    <h4>Total of Expense</h4>
+                    <p className="report minus">₭{expense}</p>
+                </div>
+            </div>
         </>
     )
 }
